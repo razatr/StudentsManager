a23dddbdc2e59878016de91965c1cb4ba8370b10
MATH RUDN <rudn.math@outlook.com>
2023-11-17 12:40:58 +0400

    added: - list of students prop to students group class, - students group view model, - students group controller, - course class

---
+++ StudentsMng/Controllers/StudentsGroupController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsManager.DAL.Context;
+using StudentsManager.ViewModels;
+
+namespace StudentsManager.Controllers
+{
+    public class StudentsGroupController : Controller
+    {
+        public StudentsDB Db { get; set; }
+        public StudentsGroupController(StudentsDB db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var groups = Db.StudentsGroups
+                .OrderBy(group => group.Id)
+                .Select(group => new StudentsGroupViewModel(group));
+
+            return View(groups);
+        }
+    }
+}
---
+++ StudentsMng/ViewModels/StudentsGroupViewModel.cs
@@ -0,0 +1,17 @@
+﻿using StudentsManager.DAL.Entities;
+using StudentsManager.DAL.Entities.Base;
+namespace StudentsManager.ViewModels;
+
+public class StudentsGroupViewModel : NamedEntity
+{
+    public string? Description { get; set; }
+    public Student[]? StudentsList { get; set; }
+
+    public StudentsGroupViewModel() { }
+
+    public StudentsGroupViewModel(StudentsGroup group)
+    {
+        Description = group.Description;
+        StudentsList = group.StudentsList.ToArray();
+    }
+}

55768d3b68adc74e2c0c74fcb587e169671ac91b
MATH RUDN <rudn.math@outlook.com>
2023-11-17 13:44:52 +0400

    modified students group view model

--- StudentsMng/ViewModels/StudentsGroupViewModel.cs
+++ StudentsMng/ViewModels/StudentsGroupViewModel.cs
@@ -4,14 +4,16 @@ namespace StudentsManager.ViewModels;
 
 public class StudentsGroupViewModel : NamedEntity
 {
-    public string? Description { get; set; }
-    public Student[]? StudentsList { get; set; }
+    public string Description { get; set; } = "Без описания";
+    public StudentViewModel[]? StudentsList { get; set; }
 
     public StudentsGroupViewModel() { }
 
     public StudentsGroupViewModel(StudentsGroup group)
     {
-        Description = group.Description;
-        StudentsList = group.StudentsList.ToArray();
+        Description = group.Description ?? "Без описания";
+        StudentsList = group.StudentsList
+            .Select(stud => new StudentViewModel(stud))
+            .ToArray();
     }
 }

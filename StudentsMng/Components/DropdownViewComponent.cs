4c943b7ecb35171133dff265a926c6eff8a36389
kaz1ch <kaz1ch@yandex.ru>
2023-11-28 17:13:12 +0300

    Added russian localisation, reworked Student View Edit, tried adding and implementing DropdownViewComponents.

---
+++ StudentsMng/Components/DropdownViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentsManager.DAL.Context;
+using StudentsManager.ViewModels;
+
+namespace StudentsManager.Components;
+
+public class DropdownViewComponent : ViewComponent
+{
+    private readonly StudentsDB _db;
+
+    public DropdownViewComponent(StudentsDB db)
+    {
+        _db = db;
+    }
+
+    public IViewComponentResult Invoke(string GroupId)
+    {
+        var groups = _db.Students
+                .Select(g => new StudentsGroupViewModel { Id = g.Id, Name = g.Name })
+                .AsEnumerable();
+
+        return View(groups);
+    }
+}
